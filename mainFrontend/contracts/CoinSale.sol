c8e0288831a406b2331cb0e34eb0f9bdf4c551dc
Dhananjay Bhavar <101885788+dhananjay-14@users.noreply.github.com>
2023-01-30 23:44:34 +0530

    Delete mainFrontend directory

--- mainFrontend/contracts/CoinSale.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.6.0;
-import "./Crowdsale.sol";
-
-contract CoinSale is Crowdsale {
-    constructor (uint256 _rate,address payable _wallet,IERC20 _token) Crowdsale(_rate,_wallet,_token)public{
-        
-    }
-}
