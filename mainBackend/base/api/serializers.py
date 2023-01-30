ef35b7a4dd91324351d29601160bc8561646f20d
Dhananjay Bhavar <101885788+dhananjay-14@users.noreply.github.com>
2023-01-30 23:44:16 +0530

    Delete mainBackend directory

--- mainBackend/base/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from base.models import Note
-
-class Noteserializer(ModelSerializer):
-    class Meta:
-        model=Note
-        fields='__all__'
--- mainBackend/base/api/serializers1.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from django.contrib.auth.models import User
-
-class serializer1(ModelSerializer):
-    class Meta:
-        model=User
-        fields=['username','password']
